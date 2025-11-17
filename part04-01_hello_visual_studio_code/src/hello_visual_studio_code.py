510e742f24286fdd0bd9962f48bcb7f7cdec395f
Marek Zelinka <mzelinka17@gmail.com>
2025-11-17 11:54:19 +0100

    Solve exercise

--- part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,1 +1,11 @@
-# Write your solution here
+while True:
+    editor = input("Editor: ").lower()
+
+    if editor == "visual studio code":
+        print("an excellent choice!")
+        break
+
+    if editor == "word" or editor == "notepad":
+        print("awful")
+    else:
+        print("not good")
