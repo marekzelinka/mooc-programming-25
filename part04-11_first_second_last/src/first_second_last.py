b63b4af655eed94b1d36df30d580e3e340df9564
Marek Zelinka <mzelinka17@gmail.com>
2025-11-17 15:29:56 +0100

    solve exercise - first second last

--- part04-11_first_second_last/src/first_second_last.py
+++ part04-11_first_second_last/src/first_second_last.py
@@ -1,5 +1,25 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+def first_word(string: str) -> str:
+    next_space_index = string.find(" ")
+
+    return string[:next_space_index]
+
+
+def second_word(string: str) -> str:
+    first_space_index = string.find(" ")
+    string = string[first_space_index + 1 :]
+    next_space_index = string.find(" ")
+    if next_space_index == -1:
+        return string
+
+    return string[:next_space_index]
+
+
+def last_word(string: str) -> str:
+    last_space = string.rfind(" ")
+
+    return string[last_space + 1 :]
+
+
 if __name__ == "__main__":
     sentence = "once upon a time there was a programmer"
     print(first_word(sentence))
