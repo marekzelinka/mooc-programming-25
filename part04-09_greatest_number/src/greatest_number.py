3aaec5c4d69220c78b461324ca7826cf0841f02e
Marek Zelinka <mzelinka17@gmail.com>
2025-11-17 15:09:02 +0100

    solve greatest number

--- part04-09_greatest_number/src/greatest_number.py
+++ part04-09_greatest_number/src/greatest_number.py
@@ -1,5 +1,12 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+def greatest_number(a: int, b: int, c: int) -> int:
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+
+
 if __name__ == "__main__":
     greatest = greatest_number(5, 4, 8)
     print(greatest)
