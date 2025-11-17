f624fa12b7fa5d764e5bf7533196a1226e23f31d
Marek Zelinka <mzelinka17@gmail.com>
2025-11-17 14:37:55 +0100

    solve spruce

--- part04-08_spruce/src/spruce.py
+++ part04-08_spruce/src/spruce.py
@@ -1,4 +1,17 @@
-# Write your solution here
-# You can test your function by calling it within the following block
+def spruce(height: int) -> None:
+    print("a spruce!")
+
+    i = 1
+
+    while i <= height:
+        spaces = height - i
+        stars = 2 * i - 1
+        print(f"{' ' * spaces}{'*' * stars}")
+
+        i += 1
+
+    print(" " * (height - 1) + "*")
+
+
 if __name__ == "__main__":
     spruce(5)
