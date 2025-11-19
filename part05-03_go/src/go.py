0cbfbcf9ebac4ea4251f989fba139cdfae67c51f
Marek Zelinka <mzelinka17@gmail.com>
2025-11-19 17:08:26 +0100

    solve exercise - go

--- part05-03_go/src/go.py
+++ part05-03_go/src/go.py
@@ -1,1 +1,32 @@
-# Write your solution here
+def count_pieces(board: list[list[int]], player: int) -> int:
+    count = 0
+
+    for row in board:
+        for col in row:
+            if col == player:
+                count += 1
+
+    return count
+
+
+def who_won(game_board: list) -> int:
+    player_a_pieces = count_pieces(game_board, 1)
+    player_b_pieces = count_pieces(game_board, 2)
+
+    if player_a_pieces == player_b_pieces:
+        return 0
+    elif player_a_pieces > player_b_pieces:
+        return 1
+    else:
+        return 2
+
+
+def main() -> None:
+    game_board = [[1, 2, 1], [0, 0, 1], [2, 1, 0]]
+    winner = who_won(game_board)
+
+    print(winner)
+
+
+if __name__ == "__main__":
+    main()
