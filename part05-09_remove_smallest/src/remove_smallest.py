bad3dc5e4d7993fe68bc5ab32c31f16f0d1d6d3d
Marek Zelinka <mzelinka17@gmail.com>
2025-11-21 14:05:45 +0100

    solve exercise - remove samllest

--- part05-09_remove_smallest/src/remove_smallest.py
+++ part05-09_remove_smallest/src/remove_smallest.py
@@ -1,1 +1,16 @@
-# Write your solution here
+def remove_smallest(numbers: list[int]) -> None:
+    smallest = min(numbers)
+
+    numbers.remove(smallest)
+
+
+def main() -> None:
+    numbers: list[int] = [2, 2, 6, 1, 3, 5]
+
+    remove_smallest(numbers)
+
+    print(numbers)  # [2, 4, 6, 3, 5]
+
+
+if __name__ == "__main__":
+    main()
