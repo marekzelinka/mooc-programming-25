373278c3f897590d82cad2de87a0668c6c3f069a
Marek Zelinka <mzelinka17@gmail.com>
2025-11-21 14:05:32 +0100

    solve exercise - items multiplied by two

--- part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
+++ part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
@@ -1,1 +1,20 @@
-# Write your solution here
+def double_items(numbers: list[int]) -> list[int]:
+    doubled: list[int] = []
+
+    for number in numbers:
+        doubled.append(number * 2)
+
+    return doubled
+
+
+def main() -> None:
+    numbers = [2, 4, 5, 3, 11, -4]
+
+    numbers_doubled = double_items(numbers)
+
+    print("original:", numbers)
+    print("doubled:", numbers_doubled)
+
+
+if __name__ == "__main__":
+    main()
