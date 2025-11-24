3b7c53bef8e9993d5aa61afe98ed79c7bd9d27c8
Marek Zelinka <mzelinka17@gmail.com>
2025-11-24 20:45:54 +0100

    format

--- part06-11_diary/src/diary.py
+++ part06-11_diary/src/diary.py
@@ -1,21 +1,19 @@
-def add_entry() -> None:
-    entry = input("Diary entry: ")
-
+def add_entry(entry: str) -> None:
     with open("diary.txt", "a") as diary:
         diary.write(f"{entry}\n")
 
+    # Handle errors and if success that print
     print("Diary saved")
 
 
 def read_entries() -> None:
-    print("Entries:")
-
     with open("diary.txt") as diary:
         for entry in diary:
             print(entry.replace("\n", ""))
 
 
 def quit() -> None:
+    # Cleanups and closing message
     print("Bye now!")
 
 
@@ -25,8 +23,12 @@ while True:
     command = int(input("Function: "))
 
     if command == 1:
-        add_entry()
+        entry = input("Diary entry: ")
+
+        add_entry(entry)
     elif command == 2:
+        print("Entries:")
+
         read_entries()
     elif command == 0:
         quit()
