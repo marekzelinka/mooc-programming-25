94826d235f38ac674b7ff8371e2eff514b1fc3b8
Marek Zelinka <mzelinka17@gmail.com>
2025-11-24 11:05:04 +0100

    solve 06-01 - largest number

--- part06-01_largest_number/src/largest_number.py
+++ part06-01_largest_number/src/largest_number.py
@@ -1,1 +1,22 @@
-# write your solution here
+def largest() -> int:
+    numbers: list[int] = []
+
+    with open("./numbers.txt") as file:
+        for line in file:
+            line = line.replace("\n", "")
+
+            number = int(line)
+            numbers.append(number)
+
+    largest_number = max(numbers)
+
+    return largest_number
+
+
+def main() -> None:
+    largest_number = largest()
+    print(largest_number)
+
+
+if __name__ == "__main__":
+    main()
