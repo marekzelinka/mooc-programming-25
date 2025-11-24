705068f325c2ae50da37296f7a47ed2dc1801091
Marek Zelinka <mzelinka17@gmail.com>
2025-11-24 20:26:08 +0100

    solve ex 06/13 - store personal data

--- part06-13_store_personal_data/src/store_personal_data.py
+++ part06-13_store_personal_data/src/store_personal_data.py
@@ -1,1 +1,5 @@
-# Write your solution here
+def store_personal_data(person: tuple[str, int, float]) -> None:
+    line = f"{person[0]};{person[1]};{person[2]}"
+
+    with open("people.csv", "a") as file:
+        file.write(f"{line}\n")
