bdd43b8db48c456f3e7cb11ec1eca6e08b8eb48c
Marek Zelinka <mzelinka17@gmail.com>
2025-11-24 13:16:29 +0100

    solve 06/04 - course grading p1

--- part06-04_course_grading_part_1/src/course_grading_part_1.py
+++ part06-04_course_grading_part_1/src/course_grading_part_1.py
@@ -1,1 +1,38 @@
-# write your solution here
+student_info = input("Student information: ")
+exercise_data = input("Exercises completed: ")
+
+
+students: dict[int, str] = {}
+
+with open(student_info) as file:
+    for line in file:
+        parts = line.split(";")
+
+        if parts[0] == "id":
+            # Ignore csv header
+            continue
+
+        id = int(parts[0])
+        first, last = parts[1:]
+
+        students[id] = f"{first} {last}".strip()
+
+exercises: dict[int, int] = {}
+
+with open(exercise_data) as file:
+    for line in file:
+        parts = line.split(";")
+
+        if parts[0] == "id":
+            # Ignore csv header
+            continue
+
+        id = int(parts[0])
+        grades = [int(grade) for grade in parts[1:]]
+
+        exercises[id] = sum(grades)
+
+for id, name in students.items():
+    completed_exercises = exercises.get(id, 0)
+
+    print(f"{name} {completed_exercises}")
