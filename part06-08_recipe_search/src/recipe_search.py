89b7fed930a924991c670bfdba9503148657f3fc
Marek Zelinka <mzelinka17@gmail.com>
2025-11-24 16:58:03 +0100

    solve ex 06/08 - recipe search

--- part06-08_recipe_search/src/recipe_search.py
+++ part06-08_recipe_search/src/recipe_search.py
@@ -1,1 +1,39 @@
-# Write your solution here
+def recipes(filename: str) -> dict:
+    recipe_list = {}
+
+    with open(filename) as file:
+        contents = file.read()
+
+        for recipe in contents.split("\n\n"):
+            name, prep_time, *ingredients = recipe.split("\n")
+
+            recipe_list[name] = {
+                "prep_time": int(prep_time),
+                "ingredients": [
+                    ingredient.strip() for ingredient in ingredients if ingredient
+                ],
+            }
+
+    return recipe_list
+
+
+def search_by_name(filename: str, word: str):
+    return [
+        recipe_name for recipe_name in recipes(filename) if word in recipe_name.lower()
+    ]
+
+
+def search_by_time(filename: str, prep_time: int):
+    return [
+        f"{recipe_name}, preparation time {recipe.get('prep_time')} min"
+        for recipe_name, recipe in recipes(filename).items()
+        if recipe.get("prep_time") <= prep_time
+    ]
+
+
+def search_by_ingredient(filename: str, ingredient: str):
+    return [
+        f"{recipe_name}, preparation time {recipe['prep_time']} min"
+        for recipe_name, recipe in recipes(filename).items()
+        if ingredient in recipe.get("ingredients", [])
+    ]
