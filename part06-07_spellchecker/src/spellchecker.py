935c2c5d86b5f62d758ac9b50ba493f3cdb33818
Marek Zelinka <mzelinka17@gmail.com>
2025-11-24 15:52:29 +0100

    solve 06/07 - spellchecker

--- part06-07_spellchecker/src/spellchecker.py
+++ part06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,26 @@
-# write your solution here
+def wordlist():
+    words: list[str] = []
+
+    with open("wordlist.txt") as file:
+        for line in file:
+            line = line.replace("\n", "")
+
+            word = line.strip()
+
+            words.append(word)
+
+    return words
+
+
+text = input("Write text: ")
+words = wordlist()
+
+for word in text.split(" "):
+    if word.lower() not in words:
+        print(f"*{word}*", end=" ")
+
+        continue
+
+    print(word, end=" ")
+
+print()
