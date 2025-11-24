0c773a1ead73c143f47f26ef3f002866d20ded5b
Marek Zelinka <mzelinka17@gmail.com>
2025-11-24 11:11:31 +0100

    solve ex 06/02 - fruit market

--- part06-02_fruit_market/src/fruit_market.py
+++ part06-02_fruit_market/src/fruit_market.py
@@ -1,1 +1,24 @@
-# write your solution here
+def read_fruits() -> dict[str, float]:
+    fruits: dict[str, float] = {}
+
+    with open("fruits.csv") as file:
+        for line in file:
+            line = line.replace("\n", "")
+
+            parts = line.split(";")
+            name = parts[0]
+            price = float(parts[1])
+
+            fruits[name] = price
+
+    return fruits
+
+
+def main() -> None:
+    fruits = read_fruits()
+
+    print(fruits)
+
+
+if __name__ == "__main__":
+    main()
