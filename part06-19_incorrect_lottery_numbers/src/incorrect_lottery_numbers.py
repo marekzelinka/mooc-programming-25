8c34be3ad7ad257ec0fcfdb7f3eb90b79a772f1a
Marek Zelinka <mzelinka17@gmail.com>
2025-11-25 14:05:10 +0100

    solve 6/19 - incorrect lottery numbers

--- part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
+++ part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
@@ -1,1 +1,48 @@
-# Write your solution here
+def parse_int(n: str) -> int:
+    try:
+        return int(n)
+    except ValueError:
+        return -1
+
+
+def filter_incorrect():
+    correct = []
+
+    with open("lottery_numbers.csv") as file:
+        for line in file:
+            line = line.replace("\n", "")
+
+            header, number_strs = line.split(";")
+
+            label, week = header.split(" ")
+
+            if label != "week":
+                continue
+
+            if parse_int(week) == -1:
+                continue
+
+            numbers = number_strs.split(",")
+
+            found_incorrect = False
+
+            for n in numbers:
+                number = parse_int(n)
+                print(f"{number}")
+
+                if not (number >= 1 and number <= 39):
+                    found_incorrect = True
+
+                    break
+
+            if found_incorrect:
+                continue
+
+            if len(numbers) < 7 or len(numbers) != len(set(numbers)):
+                continue
+
+            correct.append(line)
+
+    with open("correct_numbers.csv", "w") as file:
+        for line in correct:
+            file.write(f"{line}\n")
