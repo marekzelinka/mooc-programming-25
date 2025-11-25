3f2da3e6f03c5c667c87ac0fa6eaf74f34f1f244
Marek Zelinka <mzelinka17@gmail.com>
2025-11-25 14:04:55 +0100

    solve 6/18 - parameter validation

--- part06-18_parameter_validation/src/parameter_validation.py
+++ part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,14 @@
-# Write your solution here
+def new_person(name: str, age: int) -> tuple[str, int]:
+    if name.strip().isspace():
+        raise ValueError("Name is required")
+    elif len(name.split(" ")) < 2:
+        raise ValueError("Name must be 2 or less characters long")
+    elif len(name) > 50:
+        raise ValueError("Name must be less than 50 characters long")
+
+    if age < 0:
+        raise ValueError("Age must be greater than 0")
+    elif age > 150:
+        raise ValueError("Age must be less than 150")
+
+    return name, age
