3e011127aec45b717955dd3f4690b04ca916ddf0
Marek Zelinka <mzelinka17@gmail.com>
2025-11-26 09:45:08 +0100

    solve ex 07/09 - how old

--- part07-09_how_old/src/how_old.py
+++ part07-09_how_old/src/how_old.py
@@ -1,1 +1,14 @@
-# Write your solution here
+from datetime import datetime
+
+day = int(input("Day: "))
+month = int(input("Month: "))
+year = int(input("Year: "))
+
+date_of_birth = datetime(year, month, day)
+eve_of_millennium = datetime(1999, 12, 31)
+difference = eve_of_millennium - date_of_birth
+
+if difference.days > 0:
+    print(f"You were {difference.days} days old on the eve of the new millennium.")
+else:
+    print("You weren't born yet on the eve of the new millennium.")
