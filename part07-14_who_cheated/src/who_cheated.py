1eb5ad4b7e9af843bea52277278a1dfe79b81118
Marek Zelinka <mzelinka17@gmail.com>
2025-11-26 20:25:30 +0100

    solve ex 7/14 - who cheated

--- part07-14_who_cheated/src/who_cheated.py
+++ part07-14_who_cheated/src/who_cheated.py
@@ -1,1 +1,23 @@
-# Write your solution here
+import csv
+from datetime import datetime
+
+
+def cheaters() -> list[str]:
+    cheating_students: list[str] = []
+
+    start_times: dict[str, str] = {}
+
+    with open("start_times.csv") as file:
+        for name, time_str in csv.reader(file, delimiter=";"):
+            start_times[name] = time_str
+
+    with open("submissions.csv") as file:
+        for name, _task, _points, end_time_str in csv.reader(file, delimiter=";"):
+            start_time = datetime.strptime(start_times[name], "%H:%M")
+            end_time = datetime.strptime(end_time_str, "%H:%M")
+            diff_in_hours = (end_time - start_time).total_seconds() / 60 / 60
+
+            if diff_in_hours > 3 and name not in cheating_students:
+                cheating_students.append(name)
+
+    return cheating_students
