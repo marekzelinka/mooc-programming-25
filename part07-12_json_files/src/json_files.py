7a5194d6ac0b4a7865ea62f1e4004f3729ccefb6
Marek Zelinka <mzelinka17@gmail.com>
2025-11-26 17:07:38 +0100

    solve 7/12 - json files

--- part07-12_json_files/src/json_files.py
+++ part07-12_json_files/src/json_files.py
@@ -1,1 +1,15 @@
-# Write your solution here
+from json import loads
+
+
+def print_persons(filename: str) -> None:
+    with open(filename) as file:
+        students = loads(file.read())
+
+    for student in students:
+        print(
+            f"{student['name']} {student['age']} years ({', '.join(student['hobbies'])})"
+        )
+
+
+if __name__ == "__main__":
+    print_persons("file1.json")
