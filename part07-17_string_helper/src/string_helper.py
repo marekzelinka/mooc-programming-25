0e5df8b990d61ab79b748409ffa550a6dfbead69
Marek Zelinka <mzelinka17@gmail.com>
2025-11-27 08:56:48 +0100

    solve exercise 7/17 - string helper

--- part07-17_string_helper/src/string_helper.py
+++ part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,31 @@
-# Write your solution here
+import string
+
+
+def change_case(orig_string: str) -> str:
+    return orig_string.swapcase()
+
+
+def split_in_half(orig_string: str) -> tuple[str, str]:
+    mid_point_index = len(orig_string) // 2
+
+    return orig_string[0:mid_point_index], orig_string[mid_point_index:]
+
+
+def remove_special_characters(orig_string: str) -> str:
+    allowed = string.ascii_letters + string.digits + string.whitespace
+
+    return "".join(char for char in orig_string if char in allowed)
+
+
+if __name__ == "__main__":
+    my_string = "Well hello there!"
+
+    print(change_case(my_string))  # wELL HELLO THERE!
+
+    p1, p2 = split_in_half(my_string)
+
+    print(p1)  # Well hel
+    print(p2)  # lo there!
+
+    m2 = remove_special_characters("This is a test, lets see how it goes!!!11!")
+    print(m2)  # This is a test lets see how it goes11
