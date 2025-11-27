7e7212f348f0af3a7092714260ed675aed32ee6f
Marek Zelinka <mzelinka17@gmail.com>
2025-11-27 17:33:32 +0100

    solve 8/3 - list years

--- part08-03_list_years/src/list_years.py
+++ part08-03_list_years/src/list_years.py
@@ -1,3 +1,16 @@
 # Write your solution here
 # Remember the import statement
-# from datetime import date
+from datetime import date
+
+
+def list_years(dates: list[date]) -> list[int]:
+    return sorted([date.year for date in dates])
+
+
+if __name__ == "__main__":
+    date1 = date(2019, 2, 3)
+    date2 = date(2006, 10, 10)
+    date3 = date(1993, 5, 9)
+
+    years = list_years([date1, date2, date3])
+    print(years)  # [1993, 2006, 2019]
