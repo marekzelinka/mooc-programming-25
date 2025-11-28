bf9ae6740fdba7aca38dbedc28be9a6a0066563b
Marek Zelinka <mzelinka17@gmail.com>
2025-11-28 09:46:02 +0100

    solve 8/13 - stopwatch

--- part08-13_stopwatch/src/stopwatch.py
+++ part08-13_stopwatch/src/stopwatch.py
@@ -1,5 +1,27 @@
-# Write your solution here:
 class Stopwatch:
     def __init__(self):
-        self.seconds = 0
-        self.minutes = 0
+        self.seconds: int = 0
+        self.minutes: int = 0
+
+    def tick(self) -> None:
+        max_value = 59
+        initial = 0
+
+        if self.minutes == max_value and self.seconds == max_value:
+            self.seconds = initial
+            self.minutes = initial
+        elif self.seconds == max_value:
+            self.seconds = initial
+            self.minutes += 1
+        else:
+            self.seconds += 1
+
+    def __str__(self) -> str:
+        return f"{str(self.minutes).rjust(2, '0')}:{str(self.seconds).rjust(2, '0')}"
+
+
+if __name__ == "__main__":
+    watch = Stopwatch()
+    for i in range(3600):
+        print(watch)
+        watch.tick()
