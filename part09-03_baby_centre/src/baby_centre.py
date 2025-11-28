b6ba51f74a5b0596bd007069baa239de630a03d3
Marek Zelinka <mzelinka17@gmail.com>
2025-11-28 11:49:55 +0100

    solve 9/3

--- part09-03_baby_centre/src/baby_centre.py
+++ part09-03_baby_centre/src/baby_centre.py
@@ -1,18 +1,26 @@
-
 # WRITE YOUR SOLUTION HERE:
-#Note! Do not change the class Person!
+# Note! Do not change the class Person!
+
 
 class Person:
-    def __init__(self, name: str, age: int, height: int, weight: int):
+    def __init__(self, name: str, age: int, height: int, weight: int) -> None:
         self.name = name
         self.age = age
         self.height = height
         self.weight = weight
 
-class BabyCentre:
-    def __init__(self):
-        self.number_of_weigh_ins = 0
 
-    def weigh(self, person: Person):
-        # return the weight of the person passed as an argument
-        return -1
+class BabyCentre:
+    def __init__(self) -> None:
+        self.number_of_weigh_ins: int = 0
+
+    def weigh(self, person: Person) -> int:
+        self.number_of_weigh_ins += 1
+
+        return person.weight
+
+    def feed(self, person: Person) -> None:
+        person.weight += 1
+
+    def weigh_ins(self) -> int:
+        return self.number_of_weigh_ins
