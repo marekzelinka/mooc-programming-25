3cee9b0793ded7a3b6580073851647b8864f9b52
Marek Zelinka <mzelinka17@gmail.com>
2025-11-29 08:37:04 +0100

    solve ex 9/13 - postcodes

--- part09-13_postcodes/src/postcodes.py
+++ part09-13_postcodes/src/postcodes.py
@@ -1,9 +1,16 @@
-# Add the requested members to the class below:
-
 class City:
-    def __init__(self, name: str, population: int):
+    postcodes: dict[str, str] = {
+        "Helsinki": "00100",
+        "Turku": "20100",
+        "Tampere": "33100",
+        "Rovaniemi": "96100",
+        "Oulu": "90100",
+    }
+
+    def __init__(self, name: str, population: int, postcode: str | None = None):
         self.__name = name
         self.__population = population
+        self.postcode = postcode
 
     @property
     def name(self):
@@ -12,6 +19,26 @@ class City:
     @property
     def population(self):
         return self.__population
+
+    @property
+    def postcode(self) -> str | None:
+        return self.__postcode
+
+    @postcode.setter
+    def postcode(self, postcode: str | None) -> None:
+        if not postcode:
+            self.__postcode = None
+
+            return
+
+        postcode = City.postcodes[postcode]
+
+        if not postcode:
+            raise ValueError(
+                f"Postcode is not on the list, available options are {', '.join(City.postcodes.keys())}"
+            )
+
+        self.__postcode = postcode
 
     def __str__(self):
         return f"{self.__name} ({self.__population} residents.)"
