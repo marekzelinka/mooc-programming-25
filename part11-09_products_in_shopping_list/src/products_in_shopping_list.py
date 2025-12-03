e4e1c2b75cdf0534f643962d2227263633644ecb
Marek Zelinka <mzelinka17@gmail.com>
2025-12-03 10:22:11 +0100

    solve part 11/9 - products in shopping list

--- part11-09_products_in_shopping_list/src/products_in_shopping_list.py
+++ part11-09_products_in_shopping_list/src/products_in_shopping_list.py
@@ -1,22 +1,42 @@
 # WRITE YOUR SOLUTION HERE:
 class ShoppingList:
-    def __init__(self):
-        self.products = []
+    def __init__(self) -> None:
+        self.products: list[tuple[str, int]] = []
 
-    def number_of_items(self):
+    def number_of_items(self) -> int:
         return len(self.products)
 
-    def add(self, product: str, number: int):
+    def add(self, product: str, number: int) -> None:
         self.products.append((product, number))
 
-    def __iter__(self):
+    def __iter__(self) -> "ShoppingList":
         self.n = 0
+
         return self
 
-    def __next__(self):
-        if self.n < len(self.products):
-            product = self.products[self.n]
-            self.n += 1
-            return product
-        else:
+    def __next__(self) -> tuple[str, int]:
+        if self.n >= len(self.products):
             raise StopIteration
+
+        product = self.products[self.n]
+
+        self.n += 1
+
+        return product
+
+
+def products_in_shopping_list(shopping_list: ShoppingList, amount: int) -> list[str]:
+    return [product[0] for product in shopping_list if product[1] >= amount]
+
+
+if __name__ == "__main__":
+    my_list = ShoppingList()
+    my_list.add("bananas", 10)
+    my_list.add("apples", 5)
+    my_list.add("alcohol free beer", 24)
+    my_list.add("pineapple", 1)
+
+    print("the shopping list contains at least 8 of the following items:")
+
+    for product in products_in_shopping_list(my_list, 8):
+        print(product)
