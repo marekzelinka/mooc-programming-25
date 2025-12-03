9fe0ace276fdd3a8fc9191b5e00b2e583d6d0d0f
Marek Zelinka <mzelinka17@gmail.com>
2025-12-03 10:23:00 +0100

    solve part 11/11 - most common words

--- part11-12_most_common_words/src/most_common_words.py
+++ part11-12_most_common_words/src/most_common_words.py
@@ -1,1 +1,26 @@
 # WRITE YOUR SOLUTION HERE:
+from string import punctuation
+
+
+def most_common_words(filename: str, lower_limit: int) -> dict[str, int]:
+    with open(filename) as file:
+        contents = file.read()
+
+        # Remove newline char
+        contents = contents.replace("\n", " ")
+
+        # Cleanup any punctuations
+        for punctuation_mark in punctuation:
+            contents = contents.replace(punctuation_mark, "")
+
+        words = contents.split(" ")
+
+        return {
+            word: words.count(word)
+            for word in words
+            if words.count(word) >= lower_limit
+        }
+
+
+if __name__ == "__main__":
+    print(most_common_words("comprehensions.txt", 3))
