a12d3e7a9b72b3adc00ba538a2339723b7ebba7e
Marek Zelinka <mzelinka17@gmail.com>
2025-12-03 10:21:45 +0100

    solve part 11/7 - lottery numbers

--- part11-07_lottery_numbers/src/lottery_numbers.py
+++ part11-07_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,26 @@
 # WRITE YOUR SOLUTION HERE:
+class LotteryNumbers:
+    def __init__(self, round: int, drawn_numbers: list[int]) -> None:
+        self.round: int = round
+        self.__drawn_numbers: list[int] = drawn_numbers[:7]
+
+    def number_of_hits(self, numbers: list[int]) -> int:
+        return len([number for number in numbers if self.__hit(number)])
+
+    def hits_in_place(self, numbers: list[int]) -> list[int]:
+        return [number if self.__hit(number) else -1 for number in numbers[:7]]
+
+    def __hit(self, number: int) -> bool:
+        return number in self.__drawn_numbers
+
+
+if __name__ == "__main__":
+    week5 = LotteryNumbers(5, [1, 2, 3, 4, 5, 6, 7])
+    my_numbers = [1, 4, 7, 11, 13, 19, 24]
+
+    print(week5.number_of_hits(my_numbers))
+
+    week8 = LotteryNumbers(8, [1, 2, 3, 10, 20, 30, 33])
+    my_numbers = [1, 4, 7, 10, 11, 20, 30]
+
+    print(week8.hits_in_place(my_numbers))
