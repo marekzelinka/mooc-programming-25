b2787df013570aa78da4dda7991652287e15ebf5
Marek Zelinka <mzelinka17@gmail.com>
2025-12-04 11:46:43 +0100

    solve ex 12-09 - prime numbers

--- part12-09_prime_numbers/src/prime_numbers.py
+++ part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,20 @@
 # Write your solution here
+def is_prime(number: int):
+    return all(number % i for i in range(2, number)) if number > 1 else False
+
+
+def prime_numbers():
+    number = 1
+
+    while True:
+        if is_prime(number):
+            yield number
+
+        number += 1
+
+
+if __name__ == "__main__":
+    numbers = prime_numbers()
+
+    for i in range(62):
+        print(next(numbers))
