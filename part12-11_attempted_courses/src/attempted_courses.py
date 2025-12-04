e2e5f0bbebb121629eef1040a23e4921d88d5a0a
Marek Zelinka <mzelinka17@gmail.com>
2025-12-04 12:06:42 +0100

    solve ex 12-11 - attempted courses

--- part12-11_attempted_courses/src/attempted_courses.py
+++ part12-11_attempted_courses/src/attempted_courses.py
@@ -5,6 +5,37 @@ class CourseAttempt:
         self.grade = grade
 
     def __str__(self):
-        return f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
+        return (
+            f"{self.student_name}, grade for the course {self.course_name} {self.grade}"
+        )
+
 
 # Write your solution here
+def names_of_students(attempts: list):
+    return map(lambda attempt: attempt.student_name, attempts)
+
+
+def course_names(attempts: list):
+    return sorted(set(map(lambda attempt: attempt.course_name, attempts)))
+
+
+if __name__ == "__main__":
+    attempt = CourseAttempt("Peter Python", "Introduction to Programming", 5)
+    print(attempt.student_name)
+    print(attempt.course_name)
+    print(attempt.grade)
+    print(attempt)
+
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 2)
+
+    for name in names_of_students([s1, s2, s3]):
+        print(name)
+
+    s1 = CourseAttempt("Peter Python", "Introduction to Programming", 3)
+    s2 = CourseAttempt("Olivia C. Objective", "Introduction to Programming", 5)
+    s3 = CourseAttempt("Peter Python", "Advanced Course in Programming", 2)
+
+    for name in course_names([s1, s2, s3]):
+        print(name)
