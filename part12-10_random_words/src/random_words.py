baf1080e082910df36f23cc5acaff630fcb4f986
Marek Zelinka <mzelinka17@gmail.com>
2025-12-04 11:46:58 +0100

    solve ex 12-10 - random words

--- part12-10_random_words/src/random_words.py
+++ part12-10_random_words/src/random_words.py
@@ -1,1 +1,13 @@
 # Write your solution here:
+from random import sample
+
+
+def word_generator(characters: str, length: int, amount: int):
+    return ("".join(sample(characters, length)) for i in range(amount))
+
+
+if __name__ == "__main__":
+    wordgen = word_generator("abcdefg", 3, 5)
+
+    for word in wordgen:
+        print(word)
