1dba3105a2a338472dd90292854e16fa092b920b
Marek Zelinka <mzelinka17@gmail.com>
2025-12-04 11:46:31 +0100

    solve ex 12-08

--- part12-08_even_numbers/src/even_numbers.py
+++ part12-08_even_numbers/src/even_numbers.py
@@ -1,1 +1,23 @@
 # Write your solution here
+def even_numbers(beginning: int, maximum: int):
+    if beginning % 2 != 0:
+        beginning += 1
+
+    value = beginning
+
+    while value <= maximum:
+        yield value
+
+        value += 2
+
+
+if __name__ == "__main__":
+    numbers = even_numbers(2, 10)
+
+    for number in numbers:
+        print(number)
+
+    numbers2 = even_numbers(11, 21)
+
+    for number in numbers2:
+        print(number)
