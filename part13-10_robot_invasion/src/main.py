6c722bfc95deebc93d562a07ce1d45aa873d6edf
Marek Zelinka <mzelinka17@gmail.com>
2025-12-06 12:28:24 +0100

    refactor 13-10 - robot invasion

--- part13-10_robot_invasion/src/main.py
+++ part13-10_robot_invasion/src/main.py
@@ -3,45 +3,48 @@ from random import randint
 import pygame
 
 pygame.init()
-width, height = 640, 480
-screen = pygame.display.set_mode((width, height))
+screen_width, screen_height = 640, 480
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
 
 robot = pygame.image.load("robot.png")
-
-# number of robots (the same robots are reused)
-number = 20
-
 robots = []
-for i in range(number):
+robot_count = 20
+for i in range(robot_count):
     # causes the new random start position to be drawn immediately
-    robots.append([-1000, height])
+    robots.append([-1000, screen_height])
 
-clock = pygame.time.Clock()
-
-while True:
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit()
+            running = False
 
-    for i in range(number):
-        if robots[i][1] + robot.get_height() < height:
+    screen.fill("black")
+
+    for i in range(robot_count):
+        if robots[i][1] + robot.get_height() < screen_height:
             # the robot falls downwards
             robots[i][1] += 1
         else:
-            if robots[i][0] < -robot.get_width() or robots[i][0] > width:
+            if robots[i][0] < -robot.get_width() or robots[i][0] > screen_width:
                 # new random start point
-                robots[i][0] = randint(0, width - robot.get_width())
+                robots[i][0] = randint(0, screen_width - robot.get_width())
                 robots[i][1] = -randint(100, 1000)
-            elif robots[i][0] + robot.get_width() / 2 < width / 2:
+            elif robots[i][0] + robot.get_width() / 2 < screen_width / 2:
                 # the robot moves to the left
                 robots[i][0] -= 1
             else:
                 # the robot moves to the right
                 robots[i][0] += 1
 
-    screen.fill((0, 0, 0))
-    for i in range(number):
+    for i in range(robot_count):
         screen.blit(robot, (robots[i][0], robots[i][1]))
+
     pygame.display.flip()
 
-    clock.tick(60)
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
